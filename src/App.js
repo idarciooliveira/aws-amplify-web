460692c45013c364d4b582d16b3f1c7e6f2d0179
idarciooliveira <idarciooliveira@gmail.com>
2020-11-25 12:06:49 +0100

    setting env

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>Hello form AWS Amplify Baby</p>
+        <p>{process.env.MESSAGE}</p>
         <a
           className='App-link'
           href='https://reactjs.org'
